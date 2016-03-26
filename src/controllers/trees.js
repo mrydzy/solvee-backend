13e89bc63c18588627d11e4fc0846212fd721abd
mrydzy <majrydzy@gmail.com>
2016-03-26 21:21:39 +0100

    backend validation

--- src/controllers/trees.js
+++ src/controllers/trees.js
@@ -1,5 +1,7 @@
 'use strict';
 const RouteController = require('./route');
+const validate = require('./../services/validator').validateTree;
+const Boom = require('boom');
 
 class TreesController extends RouteController {
   constructor(server) {
@@ -19,30 +21,34 @@ class TreesController extends RouteController {
     response(tree);
   }
   save(request, response) {
-    if (request.auth.isAuthenticated) {
-      const tree = this.models.Tree.create({
-        userId: request.auth.credentials.user,
-        data: request.payload.data
-      });
-      response(tree);
-    } else {
-      response('Bad request - not authenticated');
+    const treeData = request.payload.data;
+    if (!request.auth.isAuthenticated) {
+      response(Boom.unauthorized());
     }
+    if (!validate(treeData)) {
+      response(Boom.badRequest('Tree should have title, at least 1 node and no more that 6 levels of depth'));
+    }
+    const tree = this.models.Tree.create({
+      userId: request.auth.credentials.user,
+      data: treeData
+    });
   }
   update(request, response) {
-    if (request.auth.isAuthenticated) {
-      const tree = this.models.Tree.update({
-        data: request.payload.data
-      }, {
-        where: {
-          id: request.params.treeId,
-          userId: request.auth.credentials.user
-        }
-      });
-      response(tree);
-    } else {
-      response('Bad request - not authenticated');
+    if (!request.auth.isAuthenticated) {
+      response(Boom.unauthorized());
     }
+    const treeData = request.payload.data;
+    if (!validate(treeData)) {
+      response(Boom.badRequest('Tree should have title, at least 1 node and no more that 6 levels of depth'));
+    }
+    const tree = this.models.Tree.update({
+      data: treeData
+    }, {
+      where: {
+        id: request.params.treeId,
+        userId: request.auth.credentials.user
+      }});
+      response(tree);
   }
 }
 
---
+++ src/services/validator.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const tooLongTreeIdRegex = new RegExp('id\\":([0-9]{7})+', 'g');
+
+function validateTree(treeString) {
+  var tree = JSON.parse(treeString);
+  if (tree.title.length < 1) { //no title
+    return false;
+  }
+  if (tree.options.length < 1) { //no nodes
+    return false;
+  }
+  if (tooLongTreeIdRegex.test(treeString)) { //too high, there is an id longer than 6 digits
+    return false;
+  }
+  return true;
+}
+
+module.exports = {
+  validateTree
+};
