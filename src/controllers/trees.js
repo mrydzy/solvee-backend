ca14d32c441507da5db5d676b0d0a3006e31b987
mrydzy <majrydzy@gmail.com>
2016-04-09 23:14:01 +0200

    maps list

--- src/controllers/trees.js
+++ src/controllers/trees.js
@@ -12,6 +12,10 @@ class TreesController extends RouteController {
   }
   index(request, response) {
     const trees = this.models.Tree.findAll({
+      include: [{
+        model: this.models.User,
+        attributes: ['name']
+      }],
       attributes: ['id', 'name', 'languageId', 'createdAt'],
       order: [['createdAt', 'DESC']],
       limit: indexTreesMax
